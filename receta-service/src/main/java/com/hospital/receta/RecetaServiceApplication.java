6ddf85a5670edd5c449989d9f322be1f4f039e4b
Josue A <22620229@tlaxiaco.tecnm.mx>
2026-03-25 02:16:11 -0600

    configuracion de receta, y estructura

---
+++ receta-service/src/main/java/com/hospital/receta/RecetaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.receta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecetaServiceApplication.class, args);
+    }
+
+}
